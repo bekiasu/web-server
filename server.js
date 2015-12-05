c5899e2f0e772605465ce42b399aaa072f88562b
Mohit Mehrotra <bekiasu@gmail.com>
2015-12-05 20:00:42 +0800

    separted middleware

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express')
 var app = express();
-var middleware = {
-	requireAuthentication : function(req, res, next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, resp, next) {
-		console.log('Request: '+new Date().toString()+' '+req.method+' '+req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js')
+
 // Application level middleware
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
